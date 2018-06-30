cc973df9b2498a1496b6457cbfc1b5c0a6c4cf42
Andrey Larionov <larioandr@gmail.com>
2018-06-30 16:58:43 +0300

    Adds icons to Txc1 and logs event

--- tictoc/txc1.cc
+++ tictoc/txc1.cc
@@ -30,6 +30,7 @@ void Txc1::initialize()
         // Create and send the first message on gate "out". "tictocMsg" is an
         // arbitrary string which will be the name of the message object.
         cMessage *msg = new cMessage("tictocMsg");
+        EV << "Sending initial message\n";
         send(msg, "out");
     }
 }
@@ -40,6 +41,7 @@ void Txc1::handleMessage(cMessage *msg)
     // at the module. Here, we just send it to the other module, through
     // gate `out'. Because both `tic' and `toc' does the same, the message
     // will bounce between the two.
+    EV << "Received message `" << msg->getName() << "', sending it out again\n";
     send(msg, "out");  // send out the message
 }
 
