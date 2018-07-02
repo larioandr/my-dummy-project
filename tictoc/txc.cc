438dfe989215ab8ea99b235b46821b013dd97192
Andrey Larionov <larioandr@gmail.com>
2018-07-02 18:35:08 +0300

    Avoid sending the message back via the arrival gate

--- tictoc/txc.cc
+++ tictoc/txc.cc
@@ -27,7 +27,7 @@ void Txc::initialize()
 void Txc::handleMessage(cMessage *msg)
 {
     if (getIndex() == 3) {
-        EV << "reached Tic[3], delete message\n";
+        EV << "reached Tic[" << getIndex() << "], delete message\n";
         delete msg;
     } else {
         forwardMessage(msg);
@@ -39,7 +39,14 @@ void Txc::forwardMessage(cMessage *msg)
     // In this example we just pick a random gate to send msg on.
     // We draw a random number between 0 and the size of gate `out[]'.
     int n = gateSize("out");
-    int k = intuniform(0, n-1);
+    int k;
+
+    if (msg->getArrivalGate() && gateSize("out") > 1) {
+        k = intuniform(0, n-2);
+        k = k < msg->getArrivalGate()->getIndex() ? k : k + 1;
+    } else {
+        k = intuniform(0, n-1);
+    }
 
     EV << "Forwarding message " << msg << " on port out[" << k << "]\n";
     send(msg, "out", k);
