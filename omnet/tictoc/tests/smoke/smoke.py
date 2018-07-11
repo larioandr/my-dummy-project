110c916e058ac97e46d139f23491763308f2e56b
Andrey Larionov <larioandr@gmail.com>
2018-07-11 14:58:49 +0300

    refactor and read all csv files in the folder

--- omnet/tictoc/tests/smoke/smoke.py
+++ omnet/tictoc/tests/smoke/smoke.py
@@ -6,6 +6,7 @@ import sys
 import csv
 import time
 import collections
+import glob
 import unittest
 
 
@@ -93,7 +94,6 @@ def simulate(path, config_name, cpu_time_limit):
     
     # Recording the log file
     with open(os.path.join(LOG_FILE_NAME), "a") as f:
-        f.write('#' * 80 + '\n')
         f.write(f'$ cd {result.path}\n')
         f.write(f'$ {result.command}\n\n')
         f.write(result.error_msg)
@@ -129,55 +129,71 @@ def string_to_method_name(string):
     return string
 
 
-def _build_test_method(csv_line):
-    """Builds a test_config() method.
-    CSV line must be a tuple like `(path, configname, additional_args)`
-    """
-    path, config_name, user_args = csv_line
-    path = path.strip()
-    config_name = config_name.strip()
-
-    def test_method(self):
-        result = simulate(path, config_name, CPU_TIME_LIMIT)
-        self.assertEqual(
-            result.exitcode, 0, 
-            f'config {config_name} failed, see log file'
-        )
-    test_method.__name__ = 'test_' + string_to_method_name(config_name)
-    return test_method
+SimSpec = collections.namedtuple(
+    'SimSpec', 
+    ('path', 'config_name', 'args', 'file_name', 'line_number', 'line')
+)
 
 
-def generate_test_case(csv_file_name):
-    cname = string_to_class_name('Test-' + csv_file_name)
-    tc = type(cname, (unittest.TestCase, ), {})
+def parse_csv(file_name):
 
     def filter_comments(lines):
         comment_regex = re.compile(' *#.*$')
         endline_regex = re.compile('\\n')
-        for line in lines:
+        for index, line in enumerate(lines):
             line = comment_regex.sub('', line)
             line = endline_regex.sub('', line)
             if line:
-                yield line
+                yield index + 1, line
 
-    with open(csv_file_name) as data:
-        for line in csv.reader(filter_comments(data)):
-            method = _build_test_method(line)
-            setattr(tc, method.__name__, method)
+    with open(file_name) as file:
+        for number, line in filter_comments(file):
+            path, config_name, args = list(csv.reader([line]))[0]
+            path = path.strip()
+            config_name = config_name.strip()
+            args = args.strip()
+            yield SimSpec(path, config_name, args, file_name, number, line)
 
-    return tc
+
+def _build_test_method(spec):
+    def test_method(self):
+        with open(LOG_FILE_NAME, 'a') as log_file:
+            log_file.write('#' * 80 + '\n')
+            log_file.write(
+                f'# {spec.file_name}:{spec.line_number}: {spec.line}\n'
+            )
+            log_file.write('#' * 80 + '\n')
+
+        result = simulate(spec.path, spec.config_name, CPU_TIME_LIMIT)
+        
+        self.assertEqual(
+            result.exitcode, 0, 
+            f"error at {spec.file_name}:{spec.line_number}, "
+            f"config '{spec.config_name}'"
+        )
+    test_method.__name__ = 'test_' + string_to_method_name(spec.config_name)
+    return test_method
 
 
 def load_tests(loader, tests, pattern):
     suite = unittest.TestSuite()
-    
-    # Find all CSV files
-    csv_files = ['tictocs.csv']
+
+    # Open log file to clear it    
+    with open(LOG_FILE_NAME, 'w') as dummy_file:
+        pass
+
+    # Find all CSV files        
+    csv_files = glob.glob('*.csv')
 
     # Build TestCase for each CSV-file
-    for csv_file_name in csv_files:
-        tc = generate_test_case(csv_file_name)
-        tests = loader.loadTestsFromTestCase(tc)
+    for file_name in csv_files:
+        specs = parse_csv(file_name)
+        cname = string_to_class_name('Test-' + file_name)
+        testcase = type(cname, (unittest.TestCase, ), {})
+        for spec in specs:
+            method = _build_test_method(spec)
+            setattr(testcase, method.__name__, method)
+        tests = loader.loadTestsFromTestCase(testcase)
         suite.addTests(tests)
     
     # Return the suite
@@ -186,19 +202,3 @@ def load_tests(loader, tests, pattern):
 
 if __name__ == '__main__':
     unittest.main()
-    # command = build_command('TutorialNetwork', ('.', ), "5ms")
-    # print(command)
-    # args = shlex.split(command)
-
-    # process = subprocess.Popen(
-    #     args,
-    #     cwd=ROOT_PATH,
-    #     stdout=subprocess.PIPE,    # we wouldn't like to flood the console
-    #     stderr=subprocess.STDOUT,  # join both streams
-    #     universal_newlines=True,   # we don't want to decode the stream
-    # )
-    # out = process.communicate()[0]
-    # if process.returncode:
-    #     print(out)
-
-    # print('returncode: ', process.returncode)
